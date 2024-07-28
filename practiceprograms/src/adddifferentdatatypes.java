ef75027e192f8189f4521655dd791347d533e70f
SakshamSaini1001 <sakshamsaini654@gmail.com>
2024-07-28 18:16:08 +0530

    adding numbers of different data types

---
+++ practiceprograms/src/adddifferentdatatypes.java
@@ -0,0 +1,9 @@
+public class adddifferentdatatypes {
+    public static void main(String[] args) {
+        int a = 5;
+        float b = 7.5f;
+        int k = (int) b;
+        System.out.println(a + k);
+    }
+
+}
